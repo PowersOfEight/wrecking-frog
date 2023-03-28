52f811d904c51049fac842aceba298e90f66a0c5
Dan Johnson <jdjohnson8883@gmail.com>
2023-03-28 18:31:00 -0400

    Added Horizontal Movement
    Added horizontal movement, set drag etc.

--- Wrecking Frog/Assets/Scripts/PlayerMovement.cs
+++ Wrecking Frog/Assets/Scripts/PlayerMovement.cs
@@ -6,21 +6,25 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-    private Vector2 m_movement;
+    private Rigidbody2D m_rigidBody;
+    private float m_movementX;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_rigidBody = gameObject.GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate() 
-    {
-        
+    {  
+        m_rigidBody.AddForce(Vector2.right * m_movementX  * speed );
     }
 
-    void OnMove() 
+    void OnMove(InputValue movementValue) 
     {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        m_movementX = movementVector.x;
+        
     }
     // Update is called once per frame
     void Update()
