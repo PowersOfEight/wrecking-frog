c718265d31fe4fd0d8190464027ffb06755d947e
Dan Johnson <jdjohnson8883@gmail.com>
2023-03-28 11:09:57 -0400

    Setup Input
    Installed InputSystem.

---
+++ Wrecking Frog/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Vector2 m_movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate() 
+    {
+        
+    }
+
+    void OnMove() 
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
