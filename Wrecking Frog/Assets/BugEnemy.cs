0be1bb05f2b7e67519b9e2fde15e70a098ad06ff
Bootsie Baldwin <92999871+bootsiebaldwin@users.noreply.github.com>
2023-03-30 11:02:39 -0400

    Pickup/Tadpole and Enemy
    Still needs some cleaning but there is something there.

---
+++ Wrecking Frog/Assets/BugEnemy.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The reason it is bug enemy is because it is a floating enemy that gravity doesn't affect
+/// it doesnt have to be a big necessarilly.
+/// </summary>
+
+[RequireComponent(typeof(InRoom))]
+public class BugEnemy : Enemy, IFacingMover
+{
+    [Header("Inscribed: Skeletos")]
+    public int speed = 2;
+    public float timeThinkMin = 1f;
+    public float timeThinkMax = 4f;
+
+    [Header("Dynamic: Skeletos")]
+    [Range(0, 4)]
+    public int facing = 0;
+    public float timeNextDecision = 0;
+
+    private InRoom inRm;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        inRm = GetComponent<InRoom>();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if( knockback ) return;
+
+        if (Time.time >= timeNextDecision)
+        {
+            DecideDirection();
+        }
+
+        // rigid is inherited from Enemy, which defines it in Enemy.Awake()
+        rigid.velocity = directions[facing] * speed;
+    }
+
+    void DecideDirection()
+    {
+        facing = Random.Range(0, 2);
+        timeNextDecision = Time.time + Random.Range(timeThinkMin, timeThinkMax);
+    }
+
+    //———————————————————— Implementation of IFacingMover ————————————————————
+    public int GetFacing() { // This IS different from the Dray version!!!
+        return facing % 1;
+    }
+
+    public float GetSpeed() { return speed; }
+
+    public bool moving {     // This IS different from the Dray version!!!
+        get { return (facing < 1); }
+    }
+
+    public float gridMult { get { return inRm.gridMult; } }
+
+    public bool isInRoom { get { return inRm.isInRoom; } }
+
+    /*
+    public Vector2 roomNum {
+        get { return inRm.roomNum; }
+        set { inRm.roomNum = value; }
+    }
+    */
+
+    
+    public Vector2 posInRoom
+    {
+        get { return inRm.posInRoom; }
+        set { inRm.posInRoom = value; }
+    }
+    
+
+    public Vector2 GetGridPosInRoom( float mult = -1 ) {
+        return inRm.GetGridPosInRoom( mult );
+    }
+}
