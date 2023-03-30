58495bb5cf54da5d0a4da043f7af7110b5f2ad51
Dan Johnson <jdjohnson8883@gmail.com>
2023-03-30 10:51:29 -0400

    Added jumping
    Added jumping, still need to fix double jump

--- Wrecking Frog/Assets/Scripts/PlayerMovement.cs
+++ Wrecking Frog/Assets/Scripts/PlayerMovement.cs
@@ -6,18 +6,35 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
+    public float jumpForce;
     private Rigidbody2D m_rigidBody;
+    private BoxCollider2D m_collider;
     private float m_movementX;
+    private float m_movementY;
 
     // Start is called before the first frame update
     void Start()
     {
         m_rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        m_collider = gameObject.GetComponent<BoxCollider2D>();
     }
 
     private void FixedUpdate() 
     {  
         m_rigidBody.AddForce(Vector2.right * m_movementX  * speed );
+        m_rigidBody.AddForce(Vector2.up * m_movementY);
+        m_movementY = 0;
+    }
+
+    void OnFire() 
+    {
+        Debug.Log("fired jump mechanic");
+        //  Do a box-cast downward
+        RaycastHit2D hit = Physics2D.Raycast(m_collider.transform.position, Vector2.down, m_collider.size.y / 2);
+        if(hit.collider != null ) {
+            Debug.Log("movement y enacted");
+            m_movementY = jumpForce;
+        }
     }
 
     void OnMove(InputValue movementValue) 
