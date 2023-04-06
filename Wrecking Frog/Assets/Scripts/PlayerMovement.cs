179229c98a5df0963a85d5a38c27e9940ffb41c7
Dan Johnson <jdjohnson8883@gmail.com>
2023-04-06 10:43:01 -0400

    Relative Tongue Position Complete
    Created relative tongue position ray.  Moving on to tongue state machine

--- Wrecking Frog/Assets/Scripts/PlayerMovement.cs
+++ Wrecking Frog/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,27 +10,26 @@ public class PlayerMovement : MonoBehaviour
     public Camera mainCamera;
 
     [Header("Movement Attributes")]
+    [Tooltip("The horizontal speed of player movement")]
     public float speed;
+    [Tooltip("The force of the jump")]
     public float jumpForce;
 
-    [Tooltip("The amount of tolerance needed for ray collisions")]
-    public float RayTolerance = 0.1f;
+    [Tooltip("The amount of tolerance needed to detect the ground when jumping\nThis should be a small amount")]
+    [Range(0.01f,0.1f)]
+    public float rayTolerance = 0.1f;
 
     [Header("Tongue Stuff")]
+    [Tooltip("The total length the tongue should extend")]
     public float tongueLength = 7f;
-    public float tongueSpeed = 3f;
+    [Tooltip("The amount of time, in seconds, the tongue should move outward")]
     public float tongueOutDuration = 3.0f;
     
 
-    [Header("Debugging Information")]
-    public float rayCastDurationInSeconds = 3;
-
-
-    private float m_remainingRayCastDuration;
-    private Vector2 m_currentTongueOrigin;
-    private Vector3 m_tongueEnd;
-    private Vector3 m_relativeTarget;
-
+    private float m_tongueOutTimeElapsed;
+    //  Normalized vector representing the mouse direction
+    private Vector2 m_mouseRelativeDirection;
+    private bool m_mouseIsActive;
     private LineRenderer m_line;
     private Rigidbody2D m_rigidBody;
     private BoxCollider2D m_collider;
@@ -47,48 +47,51 @@ public class PlayerMovement : MonoBehaviour
         m_line = gameObject.GetComponent<LineRenderer>();
         m_line.enabled = false;
         m_joint = gameObject.GetComponent<SpringJoint2D>();
+        m_tongueOutTimeElapsed = 0.0f;
         m_joint.enabled = false;
-        m_tongueActive = false;
+        m_mouseIsActive = false;
     }
 
     private void Update() 
     {
-        
-        if(m_remainingRayCastDuration > 0)
+        if(m_mouseIsActive && m_tongueOutTimeElapsed <= tongueOutDuration)
         {
-            m_remainingRayCastDuration -= Time.deltaTime;
-            //  Ok, so the tongue will move at tongue speed, to a maximum extension
-            //  We know we need the relative target on click to get the direction of the ray cast
-            //  However, we need to find a normalized way of doing this, so that the tongue moves out at a certain speed
-            //  and achieves a percentage of its maximum extension based on the percentage of the duration achieved
-            //  In other words, the ratio should look like this:
-            //  DrawLine(m_collider.transform.position, m_collider.transform.position + (m_relativeTarget.normalized * m_tongueOutTimeElapsed/tongueOutMaxDuration))
-            Debug.DrawLine(m_collider.transform.position,m_collider.transform.position + m_tongueEnd,Color.cyan);
-            m_tongueEnd += tongueSpeed * Time.deltaTime * m_tongueEnd.normalized;
-        
+            m_tongueOutTimeElapsed += Time.deltaTime;
+            float tongueRelativeMagnitude = tongueLength * m_tongueOutTimeElapsed / tongueOutDuration;
+            Debug.DrawRay(transform.position, tongueRelativeMagnitude * m_mouseRelativeDirection, Color.magenta);
         }
     }
     
 
     private void FixedUpdate() 
     {  
+        if( m_mouseIsActive)
+        {
+            updateMouseDirection();
+        }
         m_rigidBody.AddForce(Vector2.right * m_movementX  * speed );
         m_rigidBody.AddForce(Vector2.up * m_movementY, ForceMode2D.Impulse);
         m_movementY = 0;
-        m_line.SetPosition(0, transform.position);
-        if(m_tongueActive) 
-        {
-            // m_relativeTarget = (mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue()) - m_collider.transform.position).normalized;
-        }
-        
-        
+    
+    }
+
+    private void updateMouseDirection()
+    {
+        // Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(
+        //         Mouse.current.position.ReadValue()
+        // );
+        // m_mouseRelativeDirection = mouseWorldPosition - transform.position;
+        m_mouseRelativeDirection = (mainCamera.ScreenToWorldPoint(
+                Mouse.current.position.ReadValue()
+            ) - transform.position);
+        m_mouseRelativeDirection.Normalize();
     }
 
     void OnJump() 
     {
         // Debug.DrawRay(transform.position, Vector3.down, Color.red, 2.0f);
-        RaycastHit2D hit = Physics2D.Raycast(m_collider.transform.position, Vector2.down, (m_collider.size.y + 0.1f) / 2 , 1 << 8);
-        Debug.DrawLine(m_collider.transform.position, m_collider.transform.position + Vector3.down * (m_collider.size.y + 0.1f)/2, Color.black, 3.0f);
+        RaycastHit2D hit = Physics2D.Raycast(m_collider.transform.position, Vector2.down, (m_collider.size.y + rayTolerance) / 2 , 1 << 8);
+        Debug.DrawLine(m_collider.transform.position, m_collider.transform.position + Vector3.down * (m_collider.size.y + rayTolerance)/2, Color.black, 3.0f);
         if(hit.collider != null ) {
             m_movementY = jumpForce;
         }
@@ -99,40 +102,23 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
-    void doRayCast(Vector2 relativePosition) 
-    {
-        m_remainingRayCastDuration = rayCastDurationInSeconds;
-        m_tongueEnd = relativePosition.normalized;
-    }
+ 
 
     void OnTongue(InputValue value)
     {
         
-        if(value.isPressed)
-        {
-            m_tongueActive = true;
+            // Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(
+            //     Mouse.current.position.ReadValue());
+            // Debug.Log($"Mouse world position: {mouseWorldPosition}");
             
-            // //  TODO: logic to extend tongue when the mouse is pressed down
-            // Debug.Log($"tongue actuated...");
-            // Vector3 playerPos = gameObject.transform.position;
-            // Vector2 playerPosition = new Vector2(playerPos.x, playerPos.y);
-            Vector2 pos = Mouse.current.position.ReadValue();
-            Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-            Vector2 currentPosition = m_collider.transform.position;
-            doRayCast(mousePosition - currentPosition);
-            // m_joint.enabled = true;
-            // // Debug.DrawLine(playerPos, mousePosition,Color.green, 2.0f);
-            // m_line.SetPosition(1, mousePosition);
-            // m_line.enabled = true;
-        } 
-        else
-        {
-            m_tongueActive = false;
-            m_joint.enabled = false;
-            m_line.enabled = false;
-            Debug.Log($"Mouse has been released");
-            //  TODO: logic to retract tongue when the mouse is not pressed
-        }
+            // Vector2 mouseRelativePosition = mouseWorldPosition - gameObject.transform.position;
+            // Debug.Log($"Mouse relative position: {mouseRelativePosition}");
+            // // mouseRelativePosition.Normalize();
+            // Debug.DrawRay(transform.position, mouseRelativePosition.normalized, Color.black, 3.0f);
+            // Debug.DrawLine(transform.position, mouseWorldPosition, Color.magenta, 3.0f);
+        m_mouseIsActive = value.isPressed;
+        m_tongueOutTimeElapsed = 0.0f;
+        // updateMouseDirection();
         
     }
 
