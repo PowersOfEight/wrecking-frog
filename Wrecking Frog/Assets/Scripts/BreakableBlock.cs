4e19b98295b179e3e65f6a140b9d265eab577931
Dan Johnson <jdjohnson8883@gmail.com>
2023-04-11 08:45:13 -0400

    Cleaning up

--- Wrecking Frog/Assets/Scripts/BreakableBlock.cs
+++ Wrecking Frog/Assets/Scripts/BreakableBlock.cs
@@ -15,14 +15,6 @@ public class BreakableBlock : MonoBehaviour
     [Tooltip("Add the particle system prefab for the break animation here")]
     public GameObject particleSystemPrefab;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-
-
     private void OnCollisionEnter2D(Collision2D other) 
     {
         PlayerMovement player = other.gameObject.GetComponentInParent<PlayerMovement>();
@@ -30,21 +22,11 @@ public class BreakableBlock : MonoBehaviour
             && other.GetContact(0).normal.x != 0 
             && other.relativeVelocity.magnitude >= impactEnergyThreshold)
         {
-            // Debug.Log($"The amount of force: {other.relativeVelocity.magnitude} and the threshold is {impactEnergyThreshold}");
             Instantiate<GameObject>(particleSystemPrefab, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
         
         
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(!gameObject.active)
-        {
-            Destroy(gameObject);
-        }
     }
 }
