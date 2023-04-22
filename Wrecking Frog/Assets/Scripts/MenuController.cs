3960ef7b5dbb167d272e21ef8a959d3448d08567
reedms58 <reedms@g.cofc.edu>
2023-04-22 17:37:32 -0400

    Start and End scene
    Added scene change system. Use ints assigned to each scene in the build to switch scene.

---
+++ Wrecking Frog/Assets/Scripts/MenuController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject endPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TransitionScene(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void LoseGame()
+    {
+        endPanel.SetActive(true);
+        endPanel.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Game over...";
+    }
+
+    public void WinGame()
+    {
+        endPanel.SetActive(true);
+        endPanel.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "You win!";
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
