19f228dbe4a863d145fae2a963aea8ad6510e012
Dan Johnson <jdjohnson8883@gmail.com>
2023-04-22 21:30:26 -0400

    Tadpole blinking logic is DONE

--- Wrecking Frog/Assets/Scripts/Tadpole.cs
+++ Wrecking Frog/Assets/Scripts/Tadpole.cs
@@ -6,17 +6,30 @@ public class Tadpole : MonoBehaviour
 {
   [Tooltip("The amount of time in seconds before this instance respawns in its original location")]
   public float respawnTime = 10.0f;
+
+  [Tooltip("The percentage of the respawn time that the tadpole should start blinking")]
+  [Range(0.05f, 0.5f)]
+  public float blinkPercentage = 0.25f;
+  [Tooltip("This changes how long the blink lasts.  For example, if the number 4 is chosen, the blink will be 4 frames on, 4 frames off")]
+  [Range(1,20)]
+  public int blinkFrames = 6;
   private Vector2 m_spawnPoint;
   private Rigidbody2D m_rigidBody;
   private Rigidbody2D m_animatorRigidbody;
+  private SpriteRenderer m_renderer;
+  private Color m_originalColor;
+  private Color m_zeroAlphaColor;
+
   private Collider2D m_collider;
   private DistanceJoint2D m_parentJoint;
   private float m_respawnTimer;
+  private int m_blinkIndex;
   private enum ePickupMode
   {
     Idle,
     Collected,
-    Dropped
+    Dropped,
+    Blinking
   }
   private ePickupMode m_currentMode;
 
@@ -27,8 +40,12 @@ public class Tadpole : MonoBehaviour
     m_parentJoint = GetComponent<DistanceJoint2D>();
     m_collider = GetComponent<Collider2D>();
     m_animatorRigidbody = GetComponentInChildren<Rigidbody2D>();
+    m_renderer = GetComponentInChildren<SpriteRenderer>();
+    m_originalColor = m_renderer.color;
+    m_zeroAlphaColor = new Color(m_originalColor.r, m_originalColor.g, m_originalColor.b, 0.0f);
     m_currentMode = ePickupMode.Idle;
     m_spawnPoint = transform.position;
+    m_blinkIndex = 0;
   }
 
   private void OnTriggerEnter2D(Collider2D other) 
@@ -59,7 +76,7 @@ public class Tadpole : MonoBehaviour
     m_collider.isTrigger = true;
     m_parentJoint.enabled = false;
     m_animatorRigidbody.MoveRotation(0);
-    m_respawnTimer = respawnTime;
+    m_respawnTimer = respawnTime * (1 - blinkPercentage);
     
   }
 
@@ -79,11 +96,36 @@ public class Tadpole : MonoBehaviour
         if (m_respawnTimer <= 0)
         {
           //  respawn
-          transform.position = m_spawnPoint;
-          m_currentMode = ePickupMode.Idle;
+          m_currentMode = ePickupMode.Blinking;
+          m_respawnTimer = respawnTime * blinkPercentage;
         }
         else
         {
+          m_respawnTimer -= Time.deltaTime;
+        }
+        break;
+      case ePickupMode.Blinking:
+        if (m_respawnTimer <= 0)
+        {
+          if(transform.position.x == m_spawnPoint.x && 
+            transform.position.y == m_spawnPoint.y)
+          {
+            m_currentMode = ePickupMode.Idle;
+            m_renderer.color = m_originalColor;
+          }
+          else
+          {
+            transform.position = m_spawnPoint;
+            m_respawnTimer = respawnTime * blinkPercentage;
+          }
+        }
+        else
+        {
+          m_blinkIndex = (m_blinkIndex + 1) % blinkFrames;
+          if(m_blinkIndex == 0)
+          {
+            m_renderer.color = m_renderer.color == m_originalColor ? m_zeroAlphaColor : m_originalColor;
+          }
           m_respawnTimer -= Time.deltaTime;
         }
         break;
